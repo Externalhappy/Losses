e12241e824322db5895326e34beed5281180dc5f
External_Happy <120341215+Externalhappy@users.noreply.github.com>
2023-08-05 15:31:01 +0400

    Update losses.py

--- losses.py
+++ losses.py
@@ -15,3 +15,17 @@ def cross_entropy(logit, output):
     loss = -1/batch_size.shape[0] * (one_hot_targets * log_probs)
 
     return loss
+
+def negative_cross_entropy(logit, output):
+    
+    batch_size = logit.shape[0]
+
+    one_hot_targets = torch.zeros_like(logit)
+    one_hot_targets[torch.arange(batch_size), output.cpu().numpy()] = 1
+    
+    softmax_probs = F.softmax(logit, dim=-1)
+    log_probs = torch.log(torch.clamp(1.0 - softmax_probs, min=1e-5, max=1.) )
+    
+    loss = -1/batch_size.shape[0] * (one_hot_targets * log_probs)
+
+    return loss
