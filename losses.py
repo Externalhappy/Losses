f52905abe159aaad45e6e24f87d962d012428849
External_Happy <120341215+Externalhappy@users.noreply.github.com>
2023-08-09 15:57:36 +0400

    Update losses.py

--- losses.py
+++ losses.py
@@ -2,33 +2,27 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-def cross_entropy(logit, output):
-    
-    batch_size = logit.shape[0]
+def cross_entropy(logit, y):
 
-    one_hot_targets = torch.zeros_like(logit)
-    one_hot_targets[torch.arange(batch_size), output.cpu().numpy()] = 1
-    
+    batch_size = logit.shape[0]
     softmax_probs = F.softmax(logit, dim=-1)
     log_probs = torch.log(torch.clamp(softmax_probs, min=1e-5, max=1.) )
+    one_hot_targets = torch.zeros_like(logit)
+    for i, j in enumerate(y):
+        one_hot_targets[i, j] = 1
+    loss = - torch.sum(one_hot_targets * log_probs) / batch_size
     
-    loss = -1/batch_size * torch.sum(one_hot_targets * log_probs)
-    # loss = F.nll_loss(neg_log_probs, new_label)
-
     return loss
 
 def negative_cross_entropy(logit, output):
     
     batch_size = logit.shape[0]
-
-    one_hot_targets = torch.zeros_like(logit)
-    one_hot_targets[torch.arange(batch_size), output.cpu().numpy()] = 1
-    
     softmax_probs = F.softmax(logit, dim=-1)
-    neg_log_probs = torch.log(torch.clamp(1.0 - softmax_probs, min=1e-5, max=1.) )
-    
-    loss = -1/batch_size * torch.sum(one_hot_targets * neg_log_probs)
-    # loss = F.nll_loss(neg_log_probs, new_label)
+    log_probs = torch.log(torch.clamp(1. - softmax_probs, min=1e-5, max=1.) )
+    one_hot_targets = torch.zeros_like(logit)
+    for i, j in enumerate(y):
+        one_hot_targets[i, j] = 1
+    loss = - torch.sum(one_hot_targets * log_probs) / batch_size
 
     return loss
 
