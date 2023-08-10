3933c7a8336f256bc7e4ceda571418189ed8e80d
External_Happy <120341215+Externalhappy@users.noreply.github.com>
2023-08-10 21:35:35 +0400

    Update losses.py

--- losses.py
+++ losses.py
@@ -24,7 +24,7 @@ def weighted_samples_cross_entropy(logit, y, weights):
     one_hot_targets = torch.zeros_like(logit)
     for i, j in enumerate(y):
         one_hot_targets[i, j] = 1
-    loss = (- weights * torch.sum(one_hot_targets * log_probs, dim=1)) / batch_size
+    loss = torch.sum((- weights * torch.sum(one_hot_targets * log_probs, dim=1))) / batch_size
 
     # cross_entropy = torch.nn.CrossEntropyLoss(reduction="none")
     # loss = (weights * cross_entropy(log_probs, y)).mean()
